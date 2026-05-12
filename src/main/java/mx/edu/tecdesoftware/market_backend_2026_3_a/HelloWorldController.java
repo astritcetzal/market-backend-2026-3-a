f53404f25758e2fe321849587ee6067b3d252d4b
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-05-12 08:37:53 -0600

    test: adding HelloWorldController to test Spring

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/HelloWorldController.java
@@ -0,0 +1,15 @@
+package mx.edu.tecdesoftware.market_backend_2026_3_a;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorldController {
+    @GetMapping("/hola")
+    public String saludar() {
+        return "Hello word";
+    }
+}
+
+
